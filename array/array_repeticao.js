a95cbdc3bb07e21974c213265eb2a359d22db3f7
JoaoMendonca2017 <jpcm.engsoftware@gmail.com>
2023-03-07 17:58:25 -0300

    Código resolvido

--- array/array_repeticao.js
+++ array/array_repeticao.js
@@ -11,11 +11,10 @@ notas.push(8);
 
 let soma = 0;
 
-for (let i = 0; i < notas.length; index++) {
+for (let i = 0; i < notas.length; i++) {
     const nota = notas[i];
     soma = soma + nota;
-}
-
+} 
 
 const media = soma/notas.length;
 console.log(media);
